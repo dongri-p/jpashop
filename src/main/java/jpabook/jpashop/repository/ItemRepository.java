73c93bdd2bdaa07b76b058bafd707e3cb07b3ccd
dongri-p <hdb151@naver.com>
2026-04-07 01:38:21 +0900

    itemRepository

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.repository;
+
+@Repository
+@requiredArgsConstructor
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                .getResultList();
+    }
+    
+}
