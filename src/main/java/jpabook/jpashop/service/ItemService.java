b8eae60e73ba429f924489b3e295cf2d5115fee0
dongri-p <hdb151@naver.com>
2026-04-07 01:44:24 +0900

    itemService

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
