5b891b991f5f5214d2cb5f9a0d2fa6aad6d50e9a
dongri-p <hdb151@naver.com>
2026-04-10 01:14:02 +0900

    상품주문 테스트

---
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -0,0 +1,76 @@
+package jpabook.jpashop.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.domain.Item.Book;
+import jpabook.jpashop.repository.OrderRepository;
+
+@SpringBootTest
+@Transactional
+public class OrderServiceTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    public void 상품주문() throws Exception {
+        // given
+        Member member = new Member();
+        member.setName("회원1");
+        member.setAddress(new Address("서울", "강가", "123-123"));
+        em.persist(member);
+
+        Book book = new Book();
+        book.setName("시골 JPA");
+        book.setPrice(10000);
+        book.setStockQuantity(10);
+
+        int orderCount = 2;
+
+        // when
+        Long orderId = orderService.order(member.getId(), book.getId(), orderCount);
+
+        // then
+        Order getOrder = orderRepository.findOne(orderId);
+
+        assertEquals(OrderStatus.ORDER, getOrder.getStatus(), "상품 주문시 상태는 ORDER");
+        assertEquals(1, getOrder.getOrderItems(), "주문한 상품 종류 수가 정확해야 한다.");
+        assertEquals(10000 * orderCount, getOrder.getTotalPrice(), "주문 가격은 가격 * 수량이다.");
+        assertEquals(8, book.getStockQuantity(), "주문 수량만큼 재고가 줄어야 한다.");
+    }
+
+    @Test
+    public void 주문취소() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+
+    @Test
+    public void 상품주문_재고수량초과() throws Exception {
+        // given
+
+        // when
+
+        // then
+    }
+    
+}
