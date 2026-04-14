e0220a588b3a9a4f461aa2689c636257525ed778
dongri-p <hdb151@naver.com>
2026-04-14 19:37:07 +0900

    orderRepository 오타수정

--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -27,7 +27,7 @@ public class OrderRepository {
         return em.createQuery("select o from Order o join o.member m" + 
         "where o.status = :status " + 
         "and m.name like :name", Order.class)
-        .setParameter("statue", orderSearch.getOrderStatus())
+        .setParameter("status", orderSearch.getOrderStatus())
         .setMaxResults(1000)
         .getResultList();
     }
