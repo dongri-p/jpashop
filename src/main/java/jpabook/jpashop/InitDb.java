6226dc0f81c58ab494a21bc3cd6b4c5f878ea5bf
dongri-p <hdb151@naver.com>
2026-04-22 21:48:21 +0900

    initDb

---
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import jakarta.persistence.EntityManager;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class InitDb {
+
+    @Component
+    @Transactional
+    @RequiredArgsConstructor
+    static class InitService {
+
+        private final EntityManager em;
+        public void dvInit() {
+            Member member = new Member();
+            member.setName("username");
+            member.setAddress(new Address("서울", "1", "11111"));
+        }
+    }
+}
