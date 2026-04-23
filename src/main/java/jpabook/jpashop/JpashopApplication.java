f1804f024b1a678b9ef1e65677281511e9b8b0d1
dongri-p <hdb151@naver.com>
2026-04-23 21:44:53 +0900

    hibernate5  lazy 설정

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
+
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -14,8 +15,11 @@ public class JpashopApplication {
 	}
 
 	@Bean
-	Hibernate5Module hibernate5Module() {
-		return new Hibernate5Module();
-	}
+	Hibernate5JakartaModule hibernate5JakartaModule() {
+    	Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+		// hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+		return hibernate5JakartaModule;
+}
+
 
 }
