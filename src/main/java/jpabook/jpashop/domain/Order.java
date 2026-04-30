68514cecfe9c51357dc173ddf80e1ccb73fa142b
dongri-p <hdb151@naver.com>
2026-04-30 21:04:43 +0900

    엔티이 DTO 변환 / 페이징 한계 돌파2

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.hibernate.annotations.BatchSize;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
